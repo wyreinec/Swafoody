38f712500cb8ebdd7afac81712bb0a65e8262bc0
arasopraza <aras.opraza@gmail.com>
2022-06-18 07:47:17 +0700

    Init android project

---
+++ Swafoody-Android/app/src/main/java/com/cl/swafoody/OnBoardingActivity.kt
@@ -0,0 +1,28 @@
+package com.cl.swafoody
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.cl.swafoody.databinding.ActivityOnboardingBinding
+
+class OnboardingActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityOnboardingBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityOnboardingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, TIME)
+    }
+
+    companion object {
+        private const val TIME = 3000L
+    }
+}
